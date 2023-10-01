ff583c8d921473fc2c3907e6d5bb1901b471dc1c
Muhammed Ebrar Erkan <muhammedebrarerkan@gmail.com>
2023-10-01 16:43:14 +0300

    course.html sayfasi olusturuldu

--- edu_con/courses/admin.py
+++ edu_con/courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Course
+from . models import Course, Category
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
@@ -7,3 +7,8 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ('available',)
     search_fields = ('name', 'description')
     
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name',)}
