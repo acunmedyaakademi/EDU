86e6bb3f9bab5b15972ddcfcf9957ad70d237ab7
Muhammed Ebrar Erkan <muhammedebrarerkan@gmail.com>
2023-10-16 20:46:56 +0300

    contact sayfasi olusturuldu gönderiken form admin panelinde goruntulenebiliyor ve mesaj iletildiginde bildirim gosteriliyor

--- edu_con/pages/urls.py
+++ edu_con/pages/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from pages.views import AboutView, IndexView
+from pages.views import AboutView, IndexView, ContactView
 
 urlpatterns = [
-        path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
     # path('', views.index, name='index'),
     path('about/', AboutView.as_view(), name='about'),
     # path('about/', views.about, name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+
 
 ]
