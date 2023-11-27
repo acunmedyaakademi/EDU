905a72c9854b6600c12c9212d40fc8ee1b53eb57
Muhammed Ebrar Erkan <muhammedebrarerkan@gmail.com>
2023-11-27 17:25:09 +0300

    alinan kurslar courses da degil sadece kullanici sayfasinda listeleniyor

--- edu_con/courses/views.py
+++ edu_con/courses/views.py
@@ -23,6 +23,7 @@ def course_list(request, category_slug=None, tag_slug=None):
     tag_page = None
     categories = Category.objects.all()
     tags =  Tag.objects.all()
+    current_user = request.user
 
     if category_slug != None:
         category_page = get_object_or_404(Category, slug=category_slug)
@@ -33,7 +34,12 @@ def course_list(request, category_slug=None, tag_slug=None):
         courses = Course.objects.filter(available=True, tags=tag_page)
 
     else:
-         courses = Course.objects.all().order_by('-date')
+         #courses = Course.objects.all().order_by('-date')
+         if current_user.is_authenticated: 
+             enrolled_courses = current_user.courses_joined.all()
+             courses = Course.objects.all().order_by('-date')
+             for course in enrolled_courses:
+                 courses = courses.exclude(id = course.id)
 
     context = {
         'courses': courses,
--- edu_con/edu/settings.py
+++ edu_con/edu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+yw76c0fb#-9ezey&@x_z^4nxn_x0y07-gif^-=)bq8lgy(ng-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
